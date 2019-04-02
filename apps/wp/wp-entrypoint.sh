368efc20a6a3843a1a4b26c53322f5614e9dbbf4
Vijo Cherian <codervijo@gmail.com>
2019-04-01 17:03:08 -0700

    working Wordprss installation inside docker

--- apps/wp/wp-entrypoint.sh
+++ apps/wp/wp-entrypoint.sh
@@ -5,11 +5,12 @@ if [ -e "/root/imgb4install" ]; then
 	echo "First time run, installing wordpress"
 	service mysql start
 	mysqladmin password 'root'
-	mysql -u root --password='root' -e "CREATE USER 'wpuser' IDENTIFIED BY 'userpassword';"
-	mysql -u root --password='root' -e "CREATE DATABASE wp_database;"
-	mysql -u root --password='root' -e "GRANT ALL ON 'wp_database'.* TO 'wpuser';"
+	mysql -u root --password='root' -e "CREATE USER 'wpuser' IDENTIFIED BY 'userpasswd';"
+	mysql -u root --password='root' -e "CREATE DATABASE wpdb;"
+	mysql -u root --password='root' -e "GRANT ALL ON wpdb.* TO 'wpuser';"
 	mysql -u root --password='root' -e "FLUSH PRIVILEGES"
 
+	a2enmod rewrite
     service apache2 restart
 	rm /root/imgb4install
 fi
