4213a69cc4b4fcc68365faa866d41750290837d7
Vijo Cherian <codervijo@gmail.com>
2019-09-05 10:34:27 -0700

    More documentation in script

--- configs/firebase/firebase-entrypoint.sh
+++ configs/firebase/firebase-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Hello from inside Lamill Firebase Go docker container"
+echo "Invoke firebase as follows for login to google:"
+echo "sudo docker run -p 9005:9005 -it fbase4  firebase login"
+echo "Note a problem : cannot run multiple commands inside docker"
 
 if [ -e "/root/imgb4install" ]; then
 	echo "First time run"
