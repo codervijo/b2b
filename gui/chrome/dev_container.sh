99cf56453baa7cd667dbcb40a06a154fd4571db6
Vijo Cherian <codervijo@gmail.com>
2023-03-08 20:51:00 -0800

    Working chrome from inside docker

--- gui/chrome/dev_container.sh
+++ gui/chrome/dev_container.sh
@@ -3,4 +3,5 @@ xhost +
 
 CONTAINER=guichrome3
 sudo docker build -t ${CONTAINER} .
-sudo docker run -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v $(pwd):/src ${CONTAINER} /bin/bash
+#sudo docker run -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v $(pwd):/src ${CONTAINER} /bin/bash
+sudo docker run -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v $(pwd):/src ${CONTAINER} google-chrome-stable --no-sandbox
