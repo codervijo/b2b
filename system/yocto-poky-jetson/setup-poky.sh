f232c4efb085715ace4222afb19d1eba0d9b5f2d
Vijo Cherian <codervijo@gmail.com>
2023-05-17 16:38:25 -0700

    Fix bug in updating bblayers.con

--- system/yocto-poky-jetson/setup-poky.sh
+++ system/yocto-poky-jetson/setup-poky.sh
@@ -10,12 +10,13 @@ ls -l
 ls -l poky
 export LC_ALL="en_US.UTF-8"
 (cd poky && git submodule update --init)
-cat << EOFHERE >> $YOCTO_DIR/build/conf/bblayers.con
- BBLAYERS ?= " \
-  /home/${USER}/yocto-tegra/meta-tegra \
-  /home/${USER}/yocto-tegra/poky/meta \
-  /home/${USER}/yocto-tegra/poky/meta-poky \
-  /home/${USER}/yocto-tegra/poky/meta-yocto-bsp \
+(cd $YOCTO_DIR/poky && source oe-init-build-env build)
+cat << EOFHERE >> $YOCTO_DIR/poky/build/conf/bblayers.conf
+BBLAYERS ?= " \\
+  /home/${USER}/yocto-tegra/meta-tegra \\
+  /home/${USER}/yocto-tegra/poky/meta \\
+  /home/${USER}/yocto-tegra/poky/meta-poky \\
+  /home/${USER}/yocto-tegra/poky/meta-yocto-bsp \\
   "
 EOFHERE
 (cd $YOCTO_DIR/poky && source oe-init-build-env build && bitbake core-image-sato-dev)
