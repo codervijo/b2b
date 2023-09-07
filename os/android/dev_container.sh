fb38c551bb573a449cf326ddaae701b2afb72498
Vijo Cherian <codervijo@gmail.com>
2023-09-07 12:51:12 -0700

    Added Android OS

---
+++ os/android/dev_container.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Make sure b2b directory exists and is git clone of right project
+[ -d ./b2b ] || (echo "B2B GIT repo not found, exiting"; exit 77); [ "$?" -eq 77 ]  && exit 2
+
+CONTAINER=android2
+
+# Make symlink to docker for this dev environment
+#ln -sf b2b/apps/react-native/Dockerfile .
+
+# ToDo ;; use docker inspect instead
+if [[ "$(sudo docker images -q ${CONTAINER}:latest 2>/dev/null)" == "" ]];
+then
+    # Init container
+    sudo docker build --tag ${CONTAINER}:latest --cache-from ${CONTAINER}:latest .
+fi
+
+# Start Dev container
+sudo docker run --privileged --network=host -p19000:19000 -p19001:19001 -p19002:19002 -p19006:19006 -v${PWD}:/usr/src/app/${CONTAINER} -it ${CONTAINER}:latest  /bin/bash
+
+#docker pull kerbe/expo
+#docker run -it --rm -p 19000:19000 -p 19001:19001 -p 19002:19002 -v "$PWD:/app" \
+#-e REACT_NATIVE_PACKAGER_HOSTNAME=192.168.1.101 \
+#-e EXPO_DEVTOOLS_LISTEN_ADDRESS=0.0.0.0 \
+#--name=expo kerbe/expo start
