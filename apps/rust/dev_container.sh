81540c6a1c585b6e02e73021c9600b1e5a50b4e0
Vijo Cherian <codervijo@gmail.com>
2024-05-13 11:56:28 -0700

    Added more error checks to container shell script

--- apps/rust/dev_container.sh
+++ apps/rust/dev_container.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+B2BDIR=${HOME}/work/b2b
+PARENTDIR=${B2BDIR}/apps/rust/
+
+
 # Make sure b2b directory exists and is git clone of right project
-#[ -d ./b2b ] || (echo "B2B GIT repo not found, exiting"; exit 77); [ "$?" -eq 77 ]  && exit 2
+[ -d ${B2BDIR} ] || (echo "B2B GIT repo not found, exiting"; exit 77); [ "$?" -eq 77 ]  && exit 2
+# Check if docker exists in path
+if ! hash docker &> /dev/null
+then
+    echo "docker could not be found"
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    sudo apt install docker-ce
+    sudo usermod -aG docker ${USER}
+    #exit
+fi
 
-CONTAINER=rus1
 
-# Make symlink to docker for this dev environment
-#cp b2b/apps/react/Dockerfile .
+CONTAINER=brief1
+
+# Make dockerfile available here
+if [ ! -f Dockerfile ]
+then
+    cp ${PARENTDIR}/Dockerfile .
+fi
+
 
 # Init container
 sudo docker build --tag ${CONTAINER}:latest --cache-from ${CONTAINER}:latest -t ${CONTAINER} . || sudo docker build -t ${CONTAINER} .
