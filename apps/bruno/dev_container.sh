4cbbb1e2f9c16890f98a66e62205d19c13fd1635
Vijo Cherian <codervijo@gmail.com>
2025-04-15 23:14:52 -0700

    Add bruno docker

---
+++ apps/bruno/dev_container.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CONTAINER=wr1
+
+# Allow local docker containers to access X11 (on host)
+xhost +local:docker
+
+# Build the Docker image
+sudo docker build --tag ${CONTAINER}:latest --cache-from ${CONTAINER}:latest -t ${CONTAINER} . || sudo docker build -t ${CONTAINER} .
+
+# Start the container with X11 support
+sudo docker run \
+  -v ${PWD}:/usr/src/app/ \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e DISPLAY=$DISPLAY \
+  --env QT_X11_NO_MITSHM=1 \
+  --device /dev/fuse \
+  --cap-add SYS_ADMIN \
+  --network=host \
+  -p 6173:5173 \
+  -it ${CONTAINER} /bin/bash
+
+
